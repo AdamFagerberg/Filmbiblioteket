ff4076c59f11e3be9589cc13b4181ccb877a4ffe
AdamFagerberg <Adam.fagerberg1@gmail.com>
2024-10-20 15:09:30 +0200

    scrolling y-axis on posterlist will now scroll x-axis

--- src/components/ui/PosterContainer.jsx
+++ src/components/ui/PosterContainer.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useRef } from "react";
+
 const PosterContainer = ({ children }) => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const handleWheel = (e) => {
+      e.preventDefault();
+      container.scrollLeft += e.deltaY;
+    };
+
+    container.addEventListener("wheel", handleWheel);
+    return () => container.removeEventListener("wheel", handleWheel);
+  }, []);
+
   return (
-    <ul className="flex flex-row overflow-x-auto no-scrollbar">{children}</ul>
+    <ul
+      className="flex flex-row overflow-x-auto no-scrollbar"
+      ref={containerRef}
+    >
+      {children}
+    </ul>
   );
 };
 
